2b4b7de4405688551c2ec8703cfde420d010309c
Claire Davies <C.Davies@ucas.ac.uk>
2021-09-23 12:55:19 +0100

    rook moving vertically and horizontally

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -7,10 +7,40 @@ namespace Chessington.GameEngine.Pieces
     {
         public Rook(Player player)
             : base(player) { }
-
+        
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPosition;
+            //find current location
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+
+            //moving rook to right 
+            for (var i = 0; i < 8; i++)
+            {
+                newPosition = Square.At(currentPosition.Row, i);
+                if (!newPosition.Equals(currentPosition))
+                {
+                    availableSquares.Add(newPosition);
+                }
+               
+            }
+
+
+            for (var i = 0; i < 8; i++)
+            {
+                newPosition = Square.At(i , currentPosition.Col);
+                if (!newPosition.Equals(currentPosition))
+                {
+                    availableSquares.Add(newPosition);
+                }
+
+            }
+
+            return availableSquares;
+
+           
+
         }
     }
 }
