e798b1f3add0464712bc9a67a10da80f13b627df
Claire Davies <C.Davies@ucas.ac.uk>
2021-09-23 16:00:02 +0100

    Knight moves to the eight spaces around it

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,62 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPosition;
+            //find current location
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+
+            // move up 2 and right 1 (- 2 +1)
+            newPosition = Square.At(currentPosition.Row - 2, currentPosition.Col + 1);
+            if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+            {
+               availableSquares.Add(newPosition);
+            }
+            // move up 1 and right 2 (- 1 +2)
+            newPosition = Square.At(currentPosition.Row - 1, currentPosition.Col + 2);
+            if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+            {
+                availableSquares.Add(newPosition);
+            }
+            // move up 2 and left 1 (- 2 -1)
+            newPosition = Square.At(currentPosition.Row - 2, currentPosition.Col - 1);
+            if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+            {
+                availableSquares.Add(newPosition);
+            }
+            // move up 1 and left 2 (- 1 -2)
+            newPosition = Square.At(currentPosition.Row - 1, currentPosition.Col - 2);
+            if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+            {
+                availableSquares.Add(newPosition);
+            }
+            // move down 2 and right 1 (+ 2 +1)
+            newPosition = Square.At(currentPosition.Row + 2, currentPosition.Col + 1);
+            if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+            {
+                availableSquares.Add(newPosition);
+            }
+            // move down 1 and right 2 (+1 +2)
+            newPosition = Square.At(currentPosition.Row +1, currentPosition.Col + 2);
+            if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+            {
+                availableSquares.Add(newPosition);
+            }
+            // move down 2 and left 1 (+ 2 -1)
+            newPosition = Square.At(currentPosition.Row + 2, currentPosition.Col - 1);
+            if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+            {
+                availableSquares.Add(newPosition);
+            }
+            // move down 1 and left 2 (+ 1 -2)
+            newPosition = Square.At(currentPosition.Row + 1, currentPosition.Col - 2);
+            if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+            {
+                availableSquares.Add(newPosition);
+            }
+
+            return availableSquares;
         }
+        
     }
 }
