e789db2848f201b11e2def28c64a946757612727
Claire Davies <C.Davies@ucas.ac.uk>
2021-09-23 15:19:32 +0100

    Queen moving diagonally and horizontally and vertically

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,73 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPosition;
+            //find current location
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+
+
+            // move up and right (-+)
+            for (var i = 0; i < 8; i++)
+            {
+                newPosition = Square.At(currentPosition.Row - i, currentPosition.Col + i);
+                if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+                {
+                    availableSquares.Add(newPosition);
+                }
+            }
+            //move down and right (++)
+            for (var i = 0; i < 8; i++)
+            {
+                newPosition = Square.At(currentPosition.Row + i, currentPosition.Col + i);
+                if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+                {
+                    availableSquares.Add(newPosition);
+                }
+            }
+
+            //move down and left (+-)
+            for (var i = 0; i < 8; i++)
+            {
+                newPosition = Square.At(currentPosition.Row + i, currentPosition.Col - i);
+                if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+                {
+                    availableSquares.Add(newPosition);
+                }
+            }
+            //move up and left (--)
+            for (var i = 8; i > 0; i--)
+            {
+                newPosition = Square.At(currentPosition.Row - i, currentPosition.Col - i);
+                if (!newPosition.Equals(currentPosition) && newPosition.Row >= 0 && newPosition.Row <= 7 && newPosition.Col >= 0 && newPosition.Col <= 7)
+                {
+                    availableSquares.Add(newPosition);
+                }
+            }
+
+            //moving horizontally 
+            for (var i = 0; i < 8; i++)
+            {
+                newPosition = Square.At(currentPosition.Row, i);
+                if (!newPosition.Equals(currentPosition))
+                {
+                    availableSquares.Add(newPosition);
+                }
+
+            }
+
+            //moving vertically
+            for (var i = 0; i < 8; i++)
+            {
+                newPosition = Square.At(i, currentPosition.Col);
+                if (!newPosition.Equals(currentPosition))
+                {
+                    availableSquares.Add(newPosition);
+                }
+
+            }
+
+            return availableSquares;
         }
     }
 }
